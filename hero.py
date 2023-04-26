67ab0f35db69aa8c67d9a2d6e995226dfe1fe3e4
catmarq11 <catherine.marquezmontas@tlglearningcohort.com>
2023-04-26 19:24:27 +0000

    Learning about conditional if

--- hero.py
+++ hero.py
@@ -1,7 +1,9 @@
 warbird= {"Alter Ego": "Carol Danvers",                     
           "Origin": "Boston, Massachusetts",                           
-          "Powers": ["Super Human Strength", "Super Human Durability",
-                     "Flight", "Energy Blast",]     
-          "Relationships" : {"ally", : "Capt America", 
-                              "Enemy" : "Thanos"}
+          "Powers": ["Super Human Strength",
+                     "Super Human Durability",
+                     "Flight", 
+                     "Energy Blast"],     
+          "Relationships" : {"ally" : "Capt America", 
+                             "Enemy" : "Thanos"}
           }
