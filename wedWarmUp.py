20381fb2aa4b550a33b1ba544fa09034f6ba2236
catmarq11 <catherine.marquezmontas@tlglearningcohort.com>
2023-05-03 17:04:28 +0000

    Working with NASA API's

---
+++ wedWarmUp.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Friday Warmup | Returning Data From Complex JSON"""
+
+import requests
+import html
+import random
+
+URL = "https://opentdb.com/api.php?amount=4&category=27&difficulty=medium&type=multiple"
